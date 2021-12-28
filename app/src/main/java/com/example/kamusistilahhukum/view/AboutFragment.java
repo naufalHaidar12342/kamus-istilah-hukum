142dd98de54ac3aaa56bd0e95034e8c7a5054b3c
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-12-29 00:11:53 +0700

    add photo and text on AboutFragment, migrate cardview to androidX, remove -xlint from gradle

--- app/src/main/java/com/example/kamusistilahhukum/view/AboutFragment.java
+++ app/src/main/java/com/example/kamusistilahhukum/view/AboutFragment.java
@@ -1,36 +1,53 @@
 package com.example.kamusistilahhukum.view;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.kamusistilahhukum.R;
+import com.example.kamusistilahhukum.databinding.FragmentAboutBinding;
+import com.google.android.material.imageview.ShapeableImageView;
 
 public class AboutFragment extends Fragment {
-
-
-    private String mParam1;
-    private String mParam2;
+    FragmentAboutBinding bindingAbout;
+    ShapeableImageView img;
+    TextView aboutMe;
 
     public AboutFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        //return inflater.inflate(R.layout.fragment_about, container, false);
+
+        bindingAbout=FragmentAboutBinding.inflate(inflater,container,false);
+        View view=bindingAbout.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        img=bindingAbout.shapeableImageViewMe;
+        aboutMe=bindingAbout.tvAboutMe;
+        img.setImageResource(R.drawable.ic_launcher_foreground);
+
     }
 }
